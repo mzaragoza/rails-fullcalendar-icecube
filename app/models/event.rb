0a3a0d95a8f7217b67976d98e867ad16406f19fe
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-06-17 13:28:45 -0400

    rspec testing

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ require 'ice_cube_methods'
 class Event < ActiveRecord::Base
   include IceCubeMethods
   belongs_to :calendar
-  attr_accessible :description, :name, :calendar_id, :repeats_weekly_each_days_of_the_week_mask
+  attr_accessible :description, :name, :calendar_id
+  attr_accessible :repeats_weekly_each_days_of_the_week_mask, :repeats_monthly_each_days_of_the_month_mask, :repeats_monthly_on_ordinals_mask, :repeats_monthly_on_days_of_the_week_mask, :repeats_yearly_each_months_of_the_year_mask, :repeats_yearly_on_ordinals_mask, :repeats_yearly_on_days_of_the_week_mask
   validates_presence_of :name
   def color
     if calendar.color
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -90,4 +90,207 @@ describe Event do
                            calendar_id: calendar.id)
    expect(event.schedule.first(7)).to eq(['Mon, 17 Jun 2013 09:00:00 EDT -04:00i', 'Tue, 18 Jun 2013 09:00:00 EDT -04:00', 'Wed, 19 Jun 2013 09:00:00 EDT -04:00', 'Thu, 20 Jun 2013 09:00:00 EDT -04:00', 'Fri, 21 Jun 2013 09:00:00 EDT -04:00', 'Mon, 24 Jun 2013 09:00:00 EDT -04:00', 'Tue, 25 Jun 2013 09:00:00 EDT -04:00'])
   end
+
+  it "happends every weekend" do
+   event =  Event.create!( name: 'every weekend event',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'weekly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 65,
+                           repeats_every_n_months: nil,
+                           repeats_monthly: 'each',
+                           repeats_every_n_years: nil,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Sat, 22 Jun 2013 09:00:00 EDT -04:00', 'Sun, 23 Jun 2013 09:00:00 EDT -04:00', 'Sat, 29 Jun 2013 09:00:00 EDT -04:00', 'Sun, 30 Jun 2013 09:00:00 EDT -04:00', 'Sat, 06 Jul 2013 09:00:00 EDT -04:00'])
+  end
+
+  it "happends every Sunday" do
+   event =  Event.create!( name: 'every Sunday event',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'weekly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 1,
+                           repeats_every_n_months: nil,
+                           repeats_monthly: 'each',
+                           repeats_every_n_years: nil,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Sun, 23 Jun 2013 09:00:00 EDT -04:00', 'Sun, 30 Jun 2013 09:00:00 EDT -04:00', 'Sun, 07 Jul 2013 09:00:00 EDT -04:00', 'Sun, 14 Jul 2013 09:00:00 EDT -04:00', 'Sun, 21 Jul 2013 09:00:00 EDT -04:00'])
+  end
+
+  it "happends every Monday" do
+   event =  Event.create!( name: 'every Monday event',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'weekly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 2,
+                           repeats_every_n_months: nil,
+                           repeats_monthly: 'each',
+                           repeats_every_n_years: nil,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Mon, 17 Jun 2013 09:00:00 EDT -04:00', 'Mon, 24 Jun 2013 09:00:00 EDT -04:00', 'Mon, 01 Jul 2013 09:00:00 EDT -04:00', 'Mon, 08 Jul 2013 09:00:00 EDT -04:00', 'Mon, 15 Jul 2013 09:00:00 EDT -04:00'])
+  end
+
+  it "happends every first of the month" do
+   event =  Event.create!( name: 'the first of the month',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'monthly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 2,
+                           repeats_every_n_months: 1,
+                           repeats_monthly: 'each',
+                           repeats_monthly_each_days_of_the_month_mask: 1,
+                           repeats_monthly_on_ordinals_mask: 0,
+                           repeats_monthly_on_days_of_the_week_mask: 0,
+                           repeats_every_n_years: nil,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Mon, 01 Jul 2013 09:00:00 EDT -04:00', 'Thu, 01 Aug 2013 09:00:00 EDT -04:00', 'Sun, 01 Sep 2013 09:00:00 EDT -04:00', 'Tue, 01 Oct 2013 09:00:00 EDT -04:00', 'Fri, 01 Nov 2013 09:00:00 EDT -04:00'])
+  end
+
+  it "happends every first  and 15th of the month" do
+   event =  Event.create!( name: 'the first and 15th of the month',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'monthly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 2,
+                           repeats_every_n_months: 1,
+                           repeats_monthly: 'each',
+                           repeats_monthly_each_days_of_the_month_mask: 16385,
+                           repeats_monthly_on_ordinals_mask: 0,
+                           repeats_monthly_on_days_of_the_week_mask: 0,
+                           repeats_every_n_years: nil,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Mon, 01 Jul 2013 09:00:00 EDT -04:00', 'Mon, 15 Jul 2013 09:00:00 EDT -04:00', 'Thu, 01 Aug 2013 09:00:00 EDT -04:00', 'Thu, 15 Aug 2013 09:00:00 EDT -04:00', 'Sun, 01 Sep 2013 09:00:00 EDT -04:00'])
+  end
+
+  it "happends every second friday of the month" do
+   event =  Event.create!( name: 'the first and 15th of the month',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'monthly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 2,
+                           repeats_every_n_months: 1,
+                           repeats_monthly: 'on',
+                           repeats_monthly_each_days_of_the_month_mask: 16385,
+                           repeats_monthly_on_ordinals_mask: 2,
+                           repeats_monthly_on_days_of_the_week_mask: 32,
+                           repeats_every_n_years: nil,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Fri, 12 Jul 2013 09:00:00 EDT -04:00', 'Fri, 09 Aug 2013 09:00:00 EDT -04:00', 'Fri, 13 Sep 2013 09:00:00 EDT -04:00', 'Fri, 11 Oct 2013 09:00:00 EDT -04:00', 'Fri, 08 Nov 2013 09:00:00 EST -05:00'])
+  end
+
+  it "happends every Dicember" do
+   event =  Event.create!( name: 'a special day in dec',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'yearly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 2,
+                           repeats_every_n_months: 1,
+                           repeats_monthly: 'each',
+                           repeats_monthly_each_days_of_the_month_mask: 16385,
+                           repeats_monthly_on_ordinals_mask: 2,
+                           repeats_monthly_on_days_of_the_week_mask: 32,
+                           repeats_every_n_years: 1,
+                           repeats_yearly_each_months_of_the_year_mask: 2048,
+                           repeats_yearly_on: false,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Tue, 17 Dec 2013 09:00:00 EST -05:00', 'Wed, 17 Dec 2014 09:00:00 EST -05:00', 'Thu, 17 Dec 2015 09:00:00 EST -05:00', 'Sat, 17 Dec 2016 09:00:00 EST -05:00', 'Sun, 17 Dec 2017 09:00:00 EST -05:00'])
+  end
+
+  it "happends every third thurdsday" do
+   event =  Event.create!( name: 'a special day in dec',
+                           description: '',
+                           is_all_day: false,
+                           from_date: 'Mon, 17 Jun 2013',
+                           from_time: '2000-01-01 09:00:00 UTC',
+                           to_date: 'Mon, 17 Jun 2013',
+                           to_time: '2000-01-01 17:00:00 UTC',
+                           repeats: 'yearly',
+                           repeats_every_n_days: nil,
+                           repeats_every_n_weeks: 1,
+                           repeats_weekly_each_days_of_the_week_mask: 2,
+                           repeats_every_n_months: 1,
+                           repeats_monthly: 'each',
+                           repeats_monthly_each_days_of_the_month_mask: 16385,
+                           repeats_monthly_on_ordinals_mask: 2,
+                           repeats_monthly_on_days_of_the_week_mask: 32,
+                           repeats_every_n_years: 1,
+                           repeats_yearly_each_months_of_the_year_mask: 1024,
+                           repeats_yearly_on: true,
+                           repeats_yearly_on_ordinals_mask: 4,
+                           repeats_yearly_on_days_of_the_week_mask: 48,
+                           repeat_ends: 'never',
+                           repeat_ends_on: 'Mon, 17 Jun 2013',
+                           time_zone: 'Eastern Time (US & Canada)',
+                           calendar_id: calendar.id)
+   expect(event.schedule.first(5)).to eq(['Fri, 15 Nov 2013 09:00:00 EST -05:00i', 'Thu, 21 Nov 2013 09:00:00 EST -05:00', 'Thu, 20 Nov 2014 09:00:00 EST -05:00', 'Fri, 21 Nov 2014 09:00:00 EST -05:00', 'Thu, 19 Nov 2015 09:00:00 EST -05:00'])
+  end
 end
